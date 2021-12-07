d93d5607847594b32cc134f8306c658355d1bbae
Mahindar Boregam (HCL America Inc) <v-mabore@microsoft.com>
2021-12-06 19:43:30 -0800

    reverted

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
 	originBlacklist: originBlacklist,
 	originWhitelist: [...originWhitelist, 'https://mahindar-1.web.app', 'https://secure.splitwise.com'],
-	//   requireHeader: ['origin', 'x-requested-with'],
+	requireHeader: ['origin', 'x-requested-with'],
 	checkRateLimit: checkRateLimit,
 	removeHeaders: [
 		'cookie',
